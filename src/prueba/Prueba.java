89a2526c107c7ad82696c78f093036087ebdcea3
Alan <Alan@LAPTOP-F8KCTMJ4>
2024-05-09 21:00:39 -0600

    CORRECION

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -7,6 +7,7 @@ public class Prueba {
 
     public static void main(String[] args) {
         System.out.println("HOLA COMMIT DESDE NETBEANS");   
+        System.out.println("CORRECION DE ERRORES");
     }
     
 }
